de3ee9825f2f94306c7e8ce4fe4823e222986636
adityasharma95 <sharmaadi638@gmail.com>
2019-03-05 17:41:24 -0800

    existingfile

---
+++ num.java
@@ -0,0 +1,33 @@
+class Num
+{
+public static void main(String args [])
+{
+int numerical=2;
+int a=10;
+int b=20;
+
+switch(numerical)
+{
+	case 1: int c= a+b;
+	System.out.println(c);
+	break;
+	case 2: int d=a-b;
+	System.out.println(d);
+	break;
+	case 3: int e=a/b;
+	System.out.println(e);
+	break;
+	case 4: int f=a*b;
+	System.out.println(f);
+	break;
+	default:
+	System.out.println("not valid");
+	break;
+
+
+
+}
+
+}
+
+}
